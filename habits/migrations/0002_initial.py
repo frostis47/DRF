7037a2edb604de393fc735daf53b1f5cf4388bf9
fr0st <fr0st1s747@gmail.com>
2025-05-27 23:57:34 +0300

    init commit

--- habits/migrations/0002_initial.py
+++ habits/migrations/0002_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='habit',
             name='owner',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='Владелец'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to=settings.AUTH_USER_MODEL, verbose_name='Владелец'),
         ),
     ]
