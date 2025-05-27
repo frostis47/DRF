51b376f47f6a9acdc3c1c681dddfb41ed747e680
fr0st <fr0st1s747@gmail.com>
2025-05-27 23:58:42 +0300

    init commit

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -41,7 +41,8 @@ class Migration(migrations.Migration):
                                                                         'each of their groups.',
                                                   related_name='user_set', related_query_name='user', to='auth.group',
                                                   verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions '
+                                                                                  'for this user.',
                                                             related_name='user_set', related_query_name='user',
                                                             to='auth.permission', verbose_name='user permissions')),
             ],
